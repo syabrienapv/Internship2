adba2a29241f4ace9009e728598517b6c5559c9a
Faris-Ihsan <farisihsan32@yahoo.co.id>
2022-07-22 05:59:49 +0700

    tambah rekomendasi top ietm

--- main/prediksi.py
+++ main/prediksi.py
@@ -10,6 +10,9 @@ import os
 matrix_path = os.getcwd() + '\\main\\datas\\resep_sparse_matrix.npz'
 dataset_path = os.getcwd() + '\\main\\datas\\data.csv'
 
+
+items = []
+
 def read_sparse():
     sparse_matrix = sparse.load_npz(matrix_path)
     return sparse_matrix
@@ -19,18 +22,49 @@ def read_dataset():
     return dataset
 
 def norm_matrix():
-    return 
+    dataset = read_dataset()
+    matriks = dataset.pivot_table(index=['userId'], columns=['title'], values='rating')
+    matriks_norm = matriks.apply(lambda x: (x-np.mean(x))/(np.max(x)-np.min(x)), axis=1)
+    matriks_norm.fillna(0, inplace=True)
+    matriks_norm = matriks_norm.T
+    matriks_norm = matriks_norm.loc[:, (matriks_norm != 0).any(axis=0)]
+    return matriks_norm
 
 def item_similarity():
+    matriks_norm = norm_matrix()
     item_similarity = cosine_similarity(read_sparse())
     item_sim_dataset = pd.DataFrame(item_similarity, index = matriks_norm.index, columns = matriks_norm.index)
     return item_sim_dataset
 
-# def cosine_similarity():
-#     sm = read_sparse()
-#     item_similarity = cosine_similarity(sm)
-#     user_similarity = cosine_similarity(sm.T)
-#     return item_similarity
+def user_similarity():
+    matriks_norm = norm_matrix()
+    user_similarity = cosine_similarity(read_sparse().T)
+    user_sim_dataset = pd.DataFrame(user_similarity, index = matriks_norm.columns, columns = matriks_norm.columns)
+    return user_sim_dataset
 
+def top_item(item_title):
+    item_sim_dataset = item_similarity()
+    count = 1
+    print('item yang sama dengan {} include:\n'.format(item_title))
+    for item in item_sim_dataset.sort_values(by = item_title, ascending = False).index[1:11]:
+        print('No. {}: {}'.format(count, item))
+        items.append(item)
+        count +=1
+    return items
 
-print(read_dataset())
+# def top_users(user):
+#     matriks_norm = norm_matrix()
+#     user_sim_dataset = user_similarity()
+    
+#     if user not in matriks_norm.columns:
+#         return('tdak ada data tersedia di user {}'.format(user))
+    
+#     print('user paling mirip:\n')
+#     sim_values = user_sim_dataset.sort_values(by=user, ascending=False).loc[:,user].tolist()[1:11]
+#     sim_users = user_sim_dataset.sort_values(by=user, ascending=False).index[1:11]
+#     zipped = zip(sim_users, sim_values,)
+#     for user, sim in zipped:
+#         print('User #{0}, nilai similarity: {1:.2f}'.format(user, sim))
+#     return items
+
+print(top_users(3))
