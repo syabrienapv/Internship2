f446479f55dc986b5ba7fa0284b38d05f48cc2ce
Faris-Ihsan <farisihsan32@yahoo.co.id>
2022-07-21 21:32:02 +0700

    index.html

--- main/routes.py
+++ main/routes.py
@@ -1,8 +1,15 @@
 from unicodedata import name
 from main import app
 import os
-from flask import request, redirect, render_template
+from flask import Flask, request, redirect, render_template
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def test_form():
+    text = request.form['text']
+    processed_text = text.upper()
+    print(processed_text)
+    return render_template('index.html', teks=processed_text) 
